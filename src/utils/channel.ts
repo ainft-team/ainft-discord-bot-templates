e182fa6bcbaa8fdf97e74479a8ba558793e0c600
hyeonwoong <hyeonwoong@comcom.ai>
2022-11-16 16:52:19 +0900

    fix: remove whitelist and some unused

--- src/utils/channel.ts
+++
@@ -1,8 +0,0 @@
-import { WHITELISTED_CHANNELS } from "../common/constants"
-
-export const isWhitelistedChannel = (guildId: string, channelId: string) => {
-  const env = process.env.NODE_ENV as 'dev' | 'prod';
-  return WHITELISTED_CHANNELS[env] &&
-    WHITELISTED_CHANNELS[env][guildId] &&
-    WHITELISTED_CHANNELS[env][guildId][channelId] === true;
-}
